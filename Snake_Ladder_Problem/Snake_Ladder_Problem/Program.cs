dcc0a901cde017a1c64274fc028205328a10818c
flashfinisher7 <mvsanilkumar07@gmail.com>
2021-08-04 12:16:26 +0530

    Refactor: Checking for options

--- Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -9,9 +9,30 @@ namespace Snake_Ladder_Problem
             Console.WriteLine("This is a Snake and Ladder Problem");
             int PlayerOne = 1;
             int PlayerPosition = 0;
+            const int noplayoption = 0;
+            const int ladderoption = 1;
+            const int snakeoption = 2;
+            int positionone = 0;
             Random random = new Random();
             int RollDie = random.Next(1, 7);
             Console.WriteLine("Die is Rolls : " + RollDie);
+            int options = random.Next(0, 3);
+            switch (options)
+            {
+                case noplayoption:
+                    positionone = positionone + 0;
+                    break;
+                case ladderoption:
+                    positionone = positionone + RollDie;
+                    break;
+                case snakeoption:
+                    positionone = positionone - RollDie;
+                    if (positionone < 0)
+                    {
+                        positionone = 0;
+                    }
+                    break;
+            }
         }
     }
 }
